f4c375795449e98216ec02a74007dc672ce26801
atinosun <atinosun@yeah.net>
2019-09-08 23:46:04 +0800

    数学计算

--- 01-基础/02-Calculate/Calc.java
+++ 01-基础/02-Calculate/Calc.java
@@ -7,5 +7,6 @@ public class Calc
         System.out.println(5*6);
         System.out.println(5/6.0);
         System.out.println(5+6-9*12/4.0);
+        System.out.println((5+6-9)*12/4.0);
     }
 }
